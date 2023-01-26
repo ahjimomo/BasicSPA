9a7765749062a586af16057179e40331b4facf7e
ahjimomo <kokwoon.ng@gmail.com>
2023-01-26 21:57:38 +0800

    Updated on database.cpp and database.h under SPA

--- Code_Group_4/source/Database.cpp
+++ Code_Group_4/source/Database.cpp
@@ -4,11 +4,13 @@ sqlite3* Database::dbConnection;
 vector<vector<string>> Database::dbResults;
 char* Database::errorMessage;
 
+///// Create Table /////
 // method to connect to the database and initialize tables in the database
 void Database::initialize() {
 	// open a database connection and store the pointer into dbConnection
 	sqlite3_open("database.db", &dbConnection);
 
+	/// Procedure ///
 	// drop the existing procedure table (if any)
 	string dropProcedureTableSQL = "DROP TABLE IF EXISTS procedures";
 	sqlite3_exec(dbConnection, dropProcedureTableSQL.c_str(), NULL, 0, &errorMessage);
@@ -17,7 +19,8 @@ void Database::initialize() {
 	string createProcedureTableSQL = "CREATE TABLE procedures ( procedureName VARCHAR(255) PRIMARY KEY);";
 	sqlite3_exec(dbConnection, createProcedureTableSQL.c_str(), NULL, 0, &errorMessage);
 
-	    // drop the existing variable table (if any)
+	/// Variable ///
+	// drop the existing variable table (if any)
     string dropVariableTableSQL = "DROP TABLE IF EXISTS variables";
     sqlite3_exec(dbConnection, dropVariableTableSQL.c_str(), NULL, 0, &errorMessage);
 
@@ -25,6 +28,7 @@ void Database::initialize() {
     string createVariableTableSQL = "CREATE TABLE variables ( variableName VARCHAR(255) PRIMARY KEY);";
     sqlite3_exec(dbConnection, createVariableTableSQL.c_str(), NULL, 0, &errorMessage);
 
+	/// Constant ///
     // drop the existing constant table (if any)
     string dropConstantTableSQL = "DROP TABLE IF EXISTS constant";
     sqlite3_exec(dbConnection, dropConstantTableSQL.c_str(), NULL, 0, &errorMessage);
@@ -33,21 +37,41 @@ void Database::initialize() {
     string createConstantTableSQL = "CREATE TABLE constant( constantValue INT PRIMARY KEY);";
     sqlite3_exec(dbConnection, createConstantTableSQL.c_str(), NULL, 0, &errorMessage);
 
+	/// Assignment ///
     // drop the existing assignment table (if any)
     string dropAssignmentTableSQL = "DROP TABLE IF EXISTS assignments";
     sqlite3_exec(dbConnection, dropAssignmentTableSQL.c_str(), NULL, 0, &errorMessage);
 
     // create an assignment table
-    string createAssignmentTable = "CREATE TABLE assignment ( lines INT PRIMARY KEY);";
+    string createAssignmentTable = "CREATE TABLE assignments ( lines INT PRIMARY KEY);";
     sqlite3_exec(dbConnection, createConstantTableSQL.c_str(), NULL, 0, &errorMessage);
 
-    // drop the existing statement table (if any)
+    /// Statement ///
+	// drop the existing statement table (if any)
     string dropStatementTableSQL = "DROP TABLE IF EXISTS statements";
     sqlite3_exec(dbConnection, dropStatementTableSQL.c_str(), NULL, 0, &errorMessage);
 
     // create a statement table
     string createStatementTable = "CREATE TABLE statements ( stmtno INT PRIMARY KEY);";
     sqlite3_exec(dbConnection, createStatementTable.c_str(), NULL, 0, &errorMessage);
+
+	/// Read ///
+	// drop the existing read table (if any)
+	string dropReadTableSQL = "DROP TABLE IF EXISTS reads";
+	sqlite3_exec(dbConnection, dropReadTableSQL.c_str(), NULL, 0, &errorMessage);
+
+	// create a read table
+	string createReadTableSQL = "CREATE TABLE reads ( line VARCHAR(255) PRIMARY KEY);";
+	sqlite3_exec(dbConnection, createReadTableSQL.c_str(), NULL, 0, &errorMessage);
+
+	/// Print ///
+	// drop the existing print table (if any)
+	string dropPrintTableSQL = "DROP TABLE IF EXISTS prints";
+	sqlite3_exec(dbConnection, dropPrintTableSQL.c_str(), NULL, 0, &errorMessage);
+
+	// create a print table
+	string createPrintTableSQL = "CREATE TABLE prints ( line VARCHAR(255) PRIMARY KEY);";
+	sqlite3_exec(dbConnection, createPrintTableSQL.c_str(), NULL, 0, &errorMessage);
 
 	// initialize the result vector
 	dbResults = vector<vector<string>>();
@@ -58,12 +82,50 @@ void Database::close() {
 	sqlite3_close(dbConnection);
 }
 
+///// Insert /////
 // method to insert a procedure into the database
 void Database::insertProcedure(string procedureName) {
 	string insertProcedureSQL = "INSERT INTO procedures ('procedureName') VALUES ('" + procedureName + "');";
 	sqlite3_exec(dbConnection, insertProcedureSQL.c_str(), NULL, 0, &errorMessage);
 }
 
+// method to insert a variable into the database
+void Database::insertVariable(string variableName) {
+	string insertVariableSQL = "INSERT INTO variables ('variableName') VALUES ('" + variableName + "');";
+	sqlite3_exec(dbConnection, insertVariableSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+// method to insert a constant into the database
+void Database::insertConstant(string constantName) {
+	string insertConstantSQL = "INSERT INTO constants ('constantName') VALUES ('" + constantName + "');";
+	sqlite3_exec(dbConnection, insertConstantSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+// method to insert an assignment into the database
+void Database::insertAssignment(string assignmentLine, string lhs, string rhs) {
+	string insertAssignmentSQL = "INSERT INTO assignments ('line' , 'lhs' , 'rhs') VALUES ('" + assignmentLine + "' , '" + lhs + "' , '" + rhs + "'); ";
+	sqlite3_exec(dbConnection, insertAssignmentSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+// method to insert a statement into the database
+void Database::insertStatement(string statementLine) {
+	string insertStatementSQL = "INSERT INTO statements ('line') VALUES ('" + statementLine + "');";
+	sqlite3_exec(dbConnection, insertStatementSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+// method to insert a read into the database
+void Database::insertRead(string readLine) {
+	string insertReadSQL = "INSERT INTO reads ('line') VALUES ('" + readLine + "');";
+	sqlite3_exec(dbConnection, insertReadSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+// method to insert a print into the database
+void Database::insertPrint(string printLine) {
+	string insertPrintSQL = "INSERT INTO prints ('line') VALUES ('" + printLine + "');";
+	sqlite3_exec(dbConnection, insertPrintSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+///// Get /////
 // method to get all the procedures from the database
 void Database::getProcedures(vector<string>& results){
 	// clear the existing results
@@ -73,6 +135,114 @@ void Database::getProcedures(vector<string>& results){
 	// The callback method is only used when there are results to be returned.
 	string getProceduresSQL = "SELECT * FROM procedures;";
 	sqlite3_exec(dbConnection, getProceduresSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the variables from the database
+void Database::getVariables(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getVariableSQL = "SELECT * FROM variables;";
+	sqlite3_exec(dbConnection, getVariableSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the constants from the database
+void Database::getConstants(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getConstantSQL = "SELECT * FROM constants;";
+	sqlite3_exec(dbConnection, getConstantSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the assignment statements from the database
+void Database::getAssignments(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getAssignmentSQL = "SELECT line FROM assignments;";
+	sqlite3_exec(dbConnection, getAssignmentSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the statements from the database
+void Database::getStatements(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getStatementSQL = "SELECT * FROM statements;";
+	sqlite3_exec(dbConnection, getStatementSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the read statements from the database
+void Database::getReads(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getReadSQL = "SELECT * FROM reads;";
+	sqlite3_exec(dbConnection, getReadSQL.c_str(), callback, 0, &errorMessage);
+
+	// postprocess the results from the database so that the output is just a vector of procedure names
+	for (vector<string> dbRow : dbResults) {
+		string result;
+		result = dbRow.at(0);
+		results.push_back(result);
+	}
+}
+
+// method to get all the print statements from the database
+void Database::getPrints(vector<string>& results) {
+	// clear the existing results
+	dbResults.clear();
+
+	// retrieve the procedures from the procedure table
+	// The callback method is only used when there are results to be returned.
+	string getPrintSQL = "SELECT * FROM prints;";
+	sqlite3_exec(dbConnection, getPrintSQL.c_str(), callback, 0, &errorMessage);
 
 	// postprocess the results from the database so that the output is just a vector of procedure names
 	for (vector<string> dbRow : dbResults) {
